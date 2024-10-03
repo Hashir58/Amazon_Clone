46c33a8518b1a5e7ff7dca2088a6b673bf433660
Hashir <hashirhks@gmail.com>
2024-10-03 19:38:59 +0500

    Create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,106 @@
+class Cart {
+  cartItems;
+  localStorageKey;
+
+  constructor(key) {
+    this.localStorageKey = key;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if (!this.cartItems) {
+      this.cartItems = [
+        {
+          productId: "hammad",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "usman",
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId, selectedQuantity) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    if (!matchingItem) {
+      this.cartItems.push({
+        productId,
+        quantity: selectedQuantity,
+        deliveryOptionId: "1",
+      });
+    } else {
+      matchingItem.quantity = Number(matchingItem.quantity) + selectedQuantity;
+    }
+
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    const newCart = [];
+
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+
+    this.cartItems = newCart;
+    this.saveToStorage();
+  }
+
+  updateQuantity(productId, newQuantity) {
+    const cartItem = this.cartItems.find(
+      (item) => item.productId === productId
+    );
+    if (cartItem) {
+      cartItem.quantity = parseInt(newQuantity);
+      this.saveToStorage();
+    }
+  }
+
+  calculateCartQuantity() {
+    this.cartItems.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    this.cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    this.saveToStorage();
+  }
+}
+
+const cart = new Cart("cart-oop");
+const businessCart = new Cart("cart-business");
+
+cart.localStorageKey = "cart-oop";
+businessCart.localStorageKey = "cart-business";
+
+console.log(cart);
+console.log(businessCart);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import "../data/cart-oop.js";
+import "../data/cart-class.js";
 
 renderOrrderSummary();
 renderPaymentSummary();
